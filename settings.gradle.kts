f0e9e0ddb0ed806bb2df0d8eeaba31fa2ab73f13
Niklas Widmann <nik@somethingcatchy.net>
2024-10-28 09:53:02 +0100

    1.20.1 only build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,13 +2,11 @@ val mod_name: String by extra
 
 pluginManagement {
     repositories {
-        repositories {
-            gradlePluginPortal()
-            maven { url = uri("https://maven.fabricmc.net/") }
-            maven { url = uri("https://repo.spongepowered.org/repository/maven-public/") }
-        }
+        gradlePluginPortal()
+        maven { url = uri("https://maven.fabricmc.net/") }
+        maven { url = uri("https://repo.spongepowered.org/repository/maven-public/") }
+        maven { url = uri("https://maven.neoforged.net/releases/") }
     }
 }
 
 rootProject.name = mod_name
-include("common", "fabric", "forge")
